e1af959f1193797ddc403719555f518078c86117
Kamran550 <kamranferzeliyev01@gmail.com>
2025-05-16 15:40:17 +0400

    2 commit

--- src/pages/CreateTask.tsx
+++ src/pages/CreateTask.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import TaskForm from "../components/TaskForm";
 import { useNavigate } from "react-router-dom";
 import { createTask } from "../api/tasksApi";
--- src/pages/EditTask.tsx
+++ src/pages/EditTask.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import TaskForm from "../components/TaskForm";
 import { useNavigate, useParams } from "react-router-dom";
 import type { Task } from "../types/task";
